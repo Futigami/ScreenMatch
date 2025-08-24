3ebcc4dfcbab275267278d9c77689c4011994715
Futigami <erick.ertuito@hotmail.com>
2025-08-24 14:11:33 -0300

    Como Filmes e Series herdam de Titulo, ajustado classe inclui para receber Titulo, não sendo necessario classe sobrecarregada

--- src/br/com/futigami/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/futigami/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,18 +1,22 @@
 package br.com.futigami.screenmatch.calculos;
 
-import br.com.futigami.screenmatch.model.Filme;
-import br.com.futigami.screenmatch.model.Serie;
+import br.com.futigami.screenmatch.model.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal;
 
     //Metodo
-    public void inclui(Filme f){
-        tempoTotal += f.getDuracaoMinutos();
-    }
 
-    public void inclui(Serie s){
-        tempoTotal += s.getDuracaoMinutos();
+//    public void inclui(Filme f){
+//        tempoTotal += f.getDuracaoMinutos();
+//    }
+//
+//    public void inclui(Serie s){
+//        tempoTotal += s.getDuracaoMinutos();
+//    }
+
+    public void inclui(Titulo t){
+        tempoTotal += t.getDuracaoMinutos();
     }
 
     //Getter Setter

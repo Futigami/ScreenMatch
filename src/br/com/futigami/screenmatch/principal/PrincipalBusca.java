e4f4bbefc63e7e994d2b1c75ee6d7ce65afbba7f
Futigami <erick.ertuito@hotmail.com>
2025-09-02 14:20:35 -0300

    Criando uma request para a API OMDB utilizando bibliotecas do Java

---
+++ src/br/com/futigami/screenmatch/principal/PrincipalBusca.java
@@ -0,0 +1,28 @@
+package br.com.futigami.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = sc.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=de25df5";
+
+        HttpClient client = HttpClient.newHttpClient();
+        //Criar a request, mas necessario realizar um envio da request
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        //Para isso, criamos um HttpResponse
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        //Imprime o corpo da resposta
+        System.out.println(response.body());
+    }
+}
